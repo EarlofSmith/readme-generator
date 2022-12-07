a4f52e1c2c1eee2971a540734993150056d4039b
EarlofSmith <r1smith3333@gmail.com>
2022-12-06 21:16:16 -0700

    added input question arrays

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,69 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is the title of your project?',
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: 'Write a short discrption of your project, What was your motivation?, Why did you build this project? What problem does it solve?',
+      name: 'description',
+    },
+    {
+      type: 'input',
+      message: 'do you want a table of contents?',
+      name: 'table-of-contents',
+    },
+    {
+        type: 'input',
+        message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running?',
+        name: 'installation',
+      },
+      {
+        type: 'input',
+        message: 'Provide instructions and examples for use. Include screenshots as needed.',
+        name: 'usage',
+      },
+      {
+        type: 'input',
+        message: 'List your collaborators, if any, with links to their GitHub profiles. If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.',
+        name: 'credits',
+      },
+      {
+        type: 'input',
+        message: 'The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project.',
+        name: 'license',
+      },
+      {
+        type: 'input',
+        message: 'Badges arent necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what youre doing. ',
+        name: 'credits',
+      },
+      {
+        type: 'input',
+        message: 'If your project has a lot of features, list them here.',
+        name: 'features',
+      },
+      {
+        type: 'input',
+        message: 'Do you want to add a contributor covenant?',
+        name: 'contribute',
+      },
+  ])
+  .then((response) => {
+    fs.appendFile('response.text',JSON.stringify(response), (error) =>
+    !error ? console.log('success!')
+    : console.log('an error occured'));
+    console.log(response.name);
+    console.log(response.languages);
+    console.log(response.preferred);
+    })
+  ;
 // TODO: Create an array of questions for user input
 const questions = [];
 
